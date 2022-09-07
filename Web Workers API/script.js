e019ab429c4d8f75afaa10c1f2f31af0b6cc1a57
Chandan Ojha <chandanojha776@gmail.com>
2022-09-07 14:21:32 +0600

    Web Workers Api added

---
+++ Web Workers API/script.js
@@ -0,0 +1,35 @@
+let w;
+
+//Start Worker
+function startWorker()
+{
+  if(typeof Worker !== 'undefined'){
+    //worker available
+
+    //if w worker is not already defined
+    if(typeof w == 'undefined') {
+       //create a new worker called 'w'
+       w = new Worker("worker.js"); 
+    }
+
+    //listening for worker events/messages
+    w.onmessage = function(event){
+        document.getElementById("demo").innerHTML = event.data
+    }
+
+  } else {
+    alert("Your browser does not support web worker..!")
+  }
+}
+
+//Stop Worker
+function stopWorker()
+{
+    if(typeof Worker !== "undefined"){
+        w.terminate();
+        w = undefined;
+    } else {
+        alert("Your browser does not support web worker..!")
+    }
+
+}
